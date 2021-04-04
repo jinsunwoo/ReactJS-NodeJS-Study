602a359c36d6913d36aadb660c1b60a7c685c8bd
jinsunwoo <sun_woo__jin@naver.com>
2021-04-04 10:38:57 +0900

    Encrypted Password

--- models/User.js
+++ models/User.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const saltRounds = 10
+
 // 스키마 생성
 const userSchema = mongoose.Schema({
     name: {
@@ -10,7 +13,7 @@ const userSchema = mongoose.Schema({
         trim: true,
         unique: 1
     },
-    passwkrd: {
+    password: {
         type: String,
         maxlength: 5
     },
@@ -28,6 +31,25 @@ const userSchema = mongoose.Schema({
     },
     tokenExp: {
         type: Number
+    }
+})
+
+userSchema.pre('save', function(next) {
+    var user = this
+
+    // 비밀번호가 변경될때만 암호화 진행 (다른 필드 변경될 때는 이 작업 반복 할 필요 없으니까)
+    if(user.isModified('password')) {
+        // 비밀번호를 암호화 시킨다.
+    bcrypt.genSalt(saltRounds, function(err, salt) {
+        if(err) return next(err)
+        bcrypt.hash(user.password, salt, function(err, hash) {
+            // 패스워드를 hashed 패스워드로 바꿔줌
+            if(err) return next(err)
+            user.password = hash
+            // index.js 에서 user.save 가 next 임
+            next()
+        });
+    });
     }
 })
 
